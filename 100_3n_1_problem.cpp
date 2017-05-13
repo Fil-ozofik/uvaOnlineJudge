7b58117f6402fe8a3f0ec931a1fe704454c184c0
Éole <leo.guirinec@gmail.com>
2017-05-13 11:22:33 +0200

    :pig:  100 - The 3n + 1 Problem (new version)

--- 100_3n_1_problem.cpp
+++ 100_3n_1_problem.cpp
@@ -2,44 +2,58 @@
 100 - The 3n + 1 problem
 http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=3&page=show_problem&problem=36
 */
-#include <cstdint>
-#include <unordered_map>
+#include <iostream>
 
-static std::unordered_map<int32_t, int32_t> memory;
-
-int32_t cycle_length(int32_t n) noexcept
+int main()
 {
-    if(memory.count(n) == 1)
-		return memory[n];
+	std::ios::sync_with_stdio( false );
+	int i, j, k, l;
 
-    if(n & 1)
-    {
-        int32_t next { (n << 1) + n + 1 };
-        int32_t v { 1 + cycle_length(next) };
-        memory[n] = v;
-        return v;
-    }
-    int32_t next { n >> 1 };
-    int32_t v { 1 + cycle_length(next) };
-    memory[n] = v;
-    return v;
-}
-
-
-int main(void)
-{
-    memory[1] = 1;
-	int32_t start, end, current_max, tested;
-	while(scanf("%d %d", &start, &end) != EOF)
+	while( std::cin >> i >> j )
 	{
-		current_max = 1;
-		for(uint32_t i = start<end ? start : end, end_i = (start>end ? start : end)+1; i != end_i; ++i)
+		if( i < j )
 		{
-			tested = cycle_length(i);
-			if(tested > current_max)
-				current_max = tested;
+			k = i;
+			l = j;
 		}
-		printf("%d %d %d\n", start, end, current_max);
+		else
+		{
+			k = j;
+			l = i;
+		}
+
+		int max_cycle_length = 0;
+		int cycle_length;
+		bool validIndex = false;
+
+		while( k <= l )
+		{
+			unsigned int n = k;
+			cycle_length = 1;
+
+			while( n != 1 )
+			{
+				if( ( n & 1 ) == 1 )
+				{
+					n += ( n << 1 ) + 1;
+					n >>= 1;
+					cycle_length += 2;
+				}
+				else
+				{
+					n >>= 1;
+					++cycle_length;
+				}
+			}
+
+			if( cycle_length > max_cycle_length )
+				max_cycle_length = cycle_length;
+
+			k++;
+		}
+
+		std::cout << i << " " << j << " " << max_cycle_length << std::endl;
 	}
-    return 0;
+
+	return 0;
 }
